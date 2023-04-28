0399aebb04f8c4f96c1c26819c9b9a3c7e257d23
YatechDev <106195439+YatechDev@users.noreply.github.com>
2023-04-28 21:02:53 +0100

    Fix: Solve errors with js modals code on the subpage and add function to swap images.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,47 +2,69 @@ const toggleBtn = document.getElementById('start-btn');
 const leftBox = document.querySelector('.slider-left-box');
 const rightBox = document.querySelector('.slider-right-box');
 
-toggleBtn.addEventListener('click', function () {
-    leftBox.style.transform = 'translateX(-100%)';
-    leftBox.addEventListener('transitionend', function() {
-        leftBox.style.display = 'none';
-        rightBox.style.transform = 'translateX(-100%)';
-        rightBox.style.display = 'block';
-        setTimeout(function() {
-            rightBox.style.transform = 'translateX(0)';
-        }, 0);
-        })
-})
+if (toggleBtn) {
+    toggleBtn.addEventListener('click', function() {
+        leftBox.style.transform = 'translateX(-100%)';
+        leftBox.addEventListener('transitionend', function () {
+            leftBox.style.display = 'none';
+            rightBox.style.transform = 'translateX(-100%)';
+            rightBox.style.display = 'block';
+            setTimeout(function () {
+                rightBox.style.transform = 'translateX(0)';
+            }, 0);
+        });
+    });
+}
 
 //Rules Modal - opened in welcome div
 const modalRules = document.getElementById('modal-rules');
 const modalRulesCloseBtn = document.getElementById('modal-rules-close');
 
-document.getElementById('rules-btn').addEventListener('click', () => {
-    modalRules.classList.add('visible');
-})
+if (modalRules) {
+    document.getElementById('rules-btn').addEventListener('click', () => {
+        modalRules.classList.add('visible');
+    })
+}
 
-modalRulesCloseBtn.addEventListener('click', () => {
-    modalRules.classList.remove('visible');
-})
+if (modalRules) {
+    modalRulesCloseBtn.addEventListener('click', () => {
+        modalRules.classList.remove('visible');
+    })
+}
 
 // Rules Modal opened in select level div
-document.getElementById('rules2-btn').addEventListener('click', () => {
-    modalRules.classList.add('visible');
-})
-
-modalRulesCloseBtn.addEventListener('click', () => {
-    modalRules.classList.remove('visible');
-})
+if (modalRules) {
+    document.getElementById('rules2-btn').addEventListener('click', () => {
+        modalRules.classList.add('visible');
+    })
+}
+if (modalRules) {
+    modalRulesCloseBtn.addEventListener('click', () => {
+        modalRules.classList.remove('visible');
+    })
+}
 
 //Author Modal
 const modalAuthor = document.getElementById('modal-author');
 const modalAuthorCloseBtn = document.getElementById('modal-author-close');
 
-document.getElementById('author-btn').addEventListener('click', () => {
-    modalAuthor.classList.add('visible');
-})
+if (modalAuthor) {
+    document.getElementById('author-btn').addEventListener('click', () => {
+        modalAuthor.classList.add('visible');
+    })
+}
+if (modalAuthor) {
+    modalAuthorCloseBtn.addEventListener('click', () => {
+        modalAuthor.classList.remove('visible');
+    })
+}
 
-modalAuthorCloseBtn.addEventListener('click', () => {
-    modalAuthor.classList.remove('visible');
+const avatar = document.getElementById('playerAvatar');
+const zdjecia = document.querySelectorAll('.pokemon');
+
+zdjecia.forEach((pokemon) => {
+    pokemon.addEventListener('click', () => {
+        const src = pokemon.querySelector('img').getAttribute('src');
+        avatar.querySelector('img').setAttribute('src', src);
+    });
 })
